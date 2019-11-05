d6828d67b0b9e1141dc873501aed51c1d8aefd42
Lal Shubham <khizaruddins@gmail.com>
2019-11-05 15:45:18 +0530

    Updates

---
+++ precache-manifest.e3330f1d9fd33af15f4ff2d475e7703a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47942ef83f246223e15e563373493d50",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "b3ffdb312b3ed995dd4f",
+    "url": "/monster-rolodex/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "7de67810f839002c58ba",
+    "url": "/monster-rolodex/static/css/main.e875b813.chunk.css"
+  },
+  {
+    "revision": "b3ffdb312b3ed995dd4f",
+    "url": "/monster-rolodex/static/js/2.f8da8b08.chunk.js"
+  },
+  {
+    "revision": "7de67810f839002c58ba",
+    "url": "/monster-rolodex/static/js/main.71e02609.chunk.js"
+  },
+  {
+    "revision": "f75c77ac67bebe39c115",
+    "url": "/monster-rolodex/static/js/runtime-main.749f14a7.js"
+  }
+]);
